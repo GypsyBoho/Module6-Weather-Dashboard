faf89656ee3e93e6b5e7ed9e97976899834f59d0
BohoBabe <bohobabe@BohoBabes-MacBook-Pro.local>
2023-07-11 22:41:43 -0700

    added fetch method

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,44 @@
 // global variables that need to be stored and reference in multiple functions
+var cityInputEl = document.querySelector("#city-search");
+var searchBtn = document.querySelector("#search-btn");
+
+var APIkey = "c85ff8dc69a078677f515dbab813d971"
+
+
 function fetchWeatherData() {
-    var requestUrlOpenWeathMap = 'https://openweathermap.org/forecast5';
-// add all your URL with specific variable namnes
-// store API keys
-// element selcetors
+    var cityName = cityInputEl.value;
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}&units=metric` 
+    // Fetching the weather data
+    fetch(requestUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            // storing the weather data in a variable //
+            // var weatherData = data;
+
+            // // Access the info from the weather data //
+            // var tempurature = weatherData.main.temp;
+            // var weatherDescription = weatherData.weather[0].description;
+
+            // document.getElementById('tempurature').innerText = tempurature;
+            // document.getElementById('description').innerText = weatherDescription;
+        })
+        .catch(error => {
+            console.log('Error fetching weather data:', error);
+        });
+
 }
+
 
 // 5 day forcat
 // Map forcast
+function fiveDayForcast() {
 
-// Fetching the weather data
-fetch(requestUrl) {
-.then(response => response.json())
-        .then(data => {
-            // storing the weather data in a variable //
-            var weatherData = data;
-
-            // Access the info from the weather data //
-            var tempurature = weatherData.main.temp;
-            var weatherDescription = weatherData.weather[0].description;
-
-            document.getElementById('tempurature').innerText = tempurature;
-            document.getElementById('description').innerText = weatherDescription;
-        })
-        .cath(error => {
-            console.log('Error fetching weather data:', error);
-        });
 }
 
 
+// event listener //
+searchBtn.addEventListener("click", fetchWeatherData)
 
 
 //   .then(function (response) {
