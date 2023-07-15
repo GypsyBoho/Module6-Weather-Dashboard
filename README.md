5cae20f7113b993a9e41685de4b71b1b6cfc122c
BohoBabe <bohobabe@BohoBabes-MBP.attlocal.net>
2023-07-14 22:26:36 -0700

    push to deploy site

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Module6-Weather-Dashboard
+
+| Technology Used         | Resource URL           | 
+| ------------- |:-------------:| 
+| HTML    | [https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) | 
+| CSS     | [https://developer.mozilla.org/en-US/docs/Web/CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)      |   
+| Git | [https://git-scm.com/](https://git-scm.com/)     | 
+| jquery | [https://jquery.com/](https://jquery.com/)     |
+| API  |  [https://openweathermap.org/forecast5](https://openweathermap.org/forecast5)  |
+| API  |  [https://openweathermap.org/api/one-call-3](https://openweathermap.org/api/one-call-3)  |
+
+## Description
+
+- I wanted to create a way to easily see the current weather and forcast for a destination. Often the temperature inside is very different from outside and I want to be prepared.
+- This app at a glance gives you the current weather details such as humidity, wind and tempurature and gives you those same details in the forcast. 
+- Also there's a function that saves recent searches so if you check the weather at home often for example you don't have to type the city name in all the time. All you need to do is find the city that you recently searched for and click the name and you'll immidately bring up the most current weather and 5 day forcast.
+
+
+[Work Day Scheduler](https://gypsyboho.github.io/Module5-Scheduler/)
+
+## Usage 
+
+To use the work day scheduler 
+- You'll notice that the current date is shown automatically at the top of the page
+- Click on any time block between 9AM and 5PM and type in a note, event or other text
+- Click the save button on the right-hand-side of that row. This will save your text so that it will show the next day
+- If you are typing in some text that you don't use often, no need to save it, and when the page is refreshed the next day, that text will be gone, allowing you to type in new text without having to delete the previous text.
+- If you want to delete text some text simply click inside the hour block and delete the text.
+
+[See how it works](./Assets/WziHwI5qmb.mp4)
+
+[![See the work day scheduler in action](https://recordit.co/WziHwI5qmb)](https://recordit.co/WziHwI5qmb)
+
+## Credits
+
+I'd like to credit my classmates for help and the good documentation at w3schools and mozilla documentation.
+
+## License
+
+NA
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,157 @@
 // global variables that need to be stored and reference in multiple functions
 var cityInputEl = document.querySelector("#city-search");
 var searchBtn = document.querySelector("#search-btn");
-var weatherResultEl = document.getElementById('current-weather');
-var temperature = document.getElementById('current-temp');
+var searchHistoryEl = document.getElementById("search-history");
+var currentWeatherEl = document.getElementById("current-weather")
+var forecastCardsEl = document.getElementById("forecast-cards")
+// weatherResultEl = document.getElementById('current-weather');
+// var temperature = document.getElementById('current-temp');
+var APIkey = "29e127100ac8dc7c64308dca8be27c2f";
 
-var APIkey = "c85ff8dc69a078677f515dbab813d971"
 
+function fetchWeatherData(city) {
+    var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`;
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}&units=metric`;
 
-function fetchWeatherData() {
-    var cityName = cityInputEl.value;
-    var currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}&units=metric';
-    // var forcastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIkey}&units=metric';
-
-    // Fetching the weather data
+    // Fetching the currnent weather data for city
     fetch(currentWeatherUrl)
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            // storing the weather data in a variable //
-            // var weatherData = data;
-            var currentTemp = data.main.temp;
-            var currentHumidity = data.main.humidity
-            var currentWind = data.wind.speed
-            var currentWeatherIcon = data.weather[0];
-            document.getElementById('city-search').innerText = cityName;
-            document.getElementById('current-temp').innerText = 'City Name: ${temperature} °C';
-            document.getElementById('current-humidity').innerText = currentTemp;
-            document.getElementById('current-wind')
-            // var currentTemp = data.main.temp;
-            // var currentWind = data.wind.speed;
-            // var currentHumidity = data.main.humidity;
-            // // Access the info from the weather data //
-            // var tempurature = weatherData.main.temp;
-            // var weatherDescription = weatherData.weather[0].description;
-
-            // Show the weather result container
-            weatherResultEl.style.display = 'block';
-            fetch(forcastUrl)
-            .then(response => response.json())
-            .then(forcastData => {
-                function fiveDayForcast() {
-                
-            }})
-
+        updateCurrentWeather(data);
+        saveToSearchHistory(city);    
         })
         .catch(error => {
-            console.log('Error fetching weather data:', error);
+            console.log('Error fetching current weather data:', error);
         });
 
+        fetch(forecastUrl)
+        .then(response => response.json())
+        .then(data => {
+            updateForecast(data);
+        })
+        .catch(error => {
+            console.log("Error fetching forecast data:", error);
+        })
+}
+function updateCurrentWeather(data) {
+    currentWeatherEl.innerHTML = "";
+
+    var cityName = data.name;
+    var temperature = data.main.temp;
+    var humidity = data.main.humidity;
+    var windSpeed = data.wind.speed;
+
+    var cityNameEl = document.createElement("h1");
+    cityNameEl.textContent = cityName;
+    var temperatureEl = document.createElement("p");
+    temperatureEl.textContent = "Temperature: " + temperature + " °C";
+    var humidityEl = document.createElement("p");
+    humidityEl.textContent = "Humidity: " + humidity + "%";
+    var windEl = document.createElement("p");
+    windEl.textContent = "Wind Speed: " + windSpeed + " m/s";
+
+    currentWeatherEl.appendChild(cityNameEl);
+    currentWeatherEl.appendChild(temperatureEl);
+    currentWeatherEl.appendChild(humidityEl);
+    currentWeatherEl.appendChild(windEl);
 }
 
+function updateForecast(data) {
+    forecastCardsEl.innerHTML = "";
 
-// 5 day forcat
-// Map forcast
+var forecastList = data.list;
+if (forecastList) {
+    forecastList.slice(0, 5).forEach((forecast) => {
+            var date = forecast.dt_txt.split(" ")[0];
+            var temperature = forecast.main.temp;
+            var weatherIcon = forecast.weather[0].icon;
+            var windSpeed = forecast.wind.speed;
+            var humidity = forecast.main.humidity;
+    
+            var card = document.createElement("div");
+            card.classList.add("card");
+    
+            // create DOM elements and update content //
+            var dateEl = document.createElement("h4");
+            dateEl.textContent = date;
+            var weatherIconEl = document.createElement("img");
+            weatherIconEl.src = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+            var temperatureEl = document.createElement("p");
+            temperatureEl.textContent = "Temperature: " + temperature + " °C";
+            var windEl = document.createElement("p");
+            windEl.textContent = "Wind Speed: " + windSpeed + " m/s";
+            var humidityEl = document.createElement("p");
+            humidityEl.textContent = "Humidity: " + humidity + "%";
+        
+            // Append elements to the forecast card
+            card.appendChild(dateEl);
+            card.appendChild(weatherIconEl);
+            card.appendChild(temperatureEl);
+            card.appendChild(windEl);
+            card.appendChild(humidityEl);
+        
+            // Append forecast cards to the forecast cards container
+            forecastCardsEl.appendChild(card);
+        });
+    } else {
+        console.log("Forecast data is undefined");
+    }
 
+    };
+
+    // Function to save the city to search history and localStorage
+    function saveToSearchHistory(city) {
+      // Create a new search history item element
+      var item = document.createElement("p");
+      item.textContent = city;
+    
+      // Add click event listener to perform a new search when clicked
+      item.addEventListener("click", function() {
+        fetchWeatherData(city);
+      });
+    
+      // Append the item to the search history container
+      searchHistoryEl.appendChild(item);
+    
+      // Save the search history to localStorage
+      var searchHistory = localStorage.getItem("searchHistory");
+      if (searchHistory) {
+        searchHistory = JSON.parse(searchHistory);
+      } else {
+        searchHistory = [];
+      }
+      searchHistory.push(city);
+      localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    }
+    
+    // Function to load search history from localStorage
+    function loadSearchHistory() {
+      var searchHistory = localStorage.getItem("searchHistory");
+      if (searchHistory) {
+        searchHistory = JSON.parse(searchHistory);
+        searchHistory.forEach(city => {
+          saveToSearchHistory(city);
+        });
+      }
+    }
+    
+    // Event listener for the search button click
+    searchBtn.addEventListener("click", function() {
+      var city = cityInputEl.value;
+      if (city) {
+        fetchWeatherData(city);
+        cityInputEl.value = ""; 
+      }
+    });
+    
+    // Load search history when the page is loaded
+    window.addEventListener("load", loadSearchHistory);
+    
 // event listener //
-searchBtn.addEventListener("click", fetchWeatherData)
+// searchBtn.addEventListener("click", fetchWeatherData)
+
+
 
 
 // document.getElementById('tempurature').innerText = tempurature;
@@ -71,3 +167,56 @@ searchBtn.addEventListener("click", fetchWeatherData)
 //       console.log(data[i].name);
 //     }
 //     console.log("********");
+
+            // var currentTemp = data.main.temp;
+            // var currentWind = data.wind.speed;
+            // var currentHumidity = data.main.humidity;
+            // // Access the info from the weather data //
+            // var tempurature = weatherData.main.temp;
+            // var weatherDescription = weatherData.weather[0].description;
+
+            // var cityName = cityInputEl.value;
+    // var currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${29e127100ac8dc7c64308dca8be27c2f}&units=metric';
+    // var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIkey}&units=metric';
+    // storing the weather data in a variable //
+            // var weatherData = data;
+            // var currentTemp = data.main.temp;
+            // var currentHumidity = data.main.humidity
+            // var currentWind = data.wind.speed
+            // var currentWeatherIcon = data.weather[0];
+            // document.getElementById('city-search').innerText = cityName;
+            // document.getElementById('current-temp').innerText = 'City Name: ${temperature} °C';
+            // document.getElementById('current-humidity').innerText = currentTemp;
+            // document.getElementById('current-wind')
+
+
+            // // Show the weather result container
+            // weatherResultEl.style.display = 'block';
+            // var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q={cityName}&appid={APIkey}&units=metric';
+            // // var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIkey}&units=metric`;
+            // fetch(forecastUrl)
+            //     .then(response => response.json())
+            //     .then(forecastData => {
+            //         function fiveDayForecast(forecastData) {
+            //             var forecastContainer = document.getElementById('five-forecast');
+            //             // clears content in string if any
+            //             forecastContainer.innerHTML = '';
+            //             for (var i = 0; i < forecastData.list.length; ++i) {
+            //                 var forecast = forecastData.list[i];
+            //                 var date = forecast.dt_txt;
+            //                 var temperature = forecast.main.temp;
+            //                 var weatherDescription = forcast.weather[0].description;
+            //                 // var forecastElement.classList.add('forecast-day');
+            //                 var forecastElement = document.createElement('div');
+            //                 forecastElements.classList.add('forcast-day');
+            //                 var weatherDescriptionElement = document.createElement('div')
+            //                 var dateElement = document.createElement('h4');
+            //                 dateElement.textContent = date;
+            //                 forecastElement.appendChild(dateElement); 
+                            
+            //             }
+            //         }
+            //     })
+            //     .catch(error => {
+            //         console.log('Error fetching forcast data', error);
+            //     })
